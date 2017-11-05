56878acf20e79aedaa50bb6270bff80bf6298557
MatejMecka <matej.plavevski+github@gmail.com>
2017-11-05 20:04:33 +0100

    Fix bugs

--- bot.py
+++ bot.py
@@ -24,33 +24,45 @@ auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
 def index_value(level):
-	level = round(float(level))
-	if level > 50 and level < 75:
-		return "Среден"
-	elif  level > 76 and level < 100:
-		return "Високо"
-	elif level > 101:
-		return "Многу високо"
+    level = round(float(level))
+    if level >= 50 and level <= 75:
+        return "Среден"
+    elif  level >= 76 and level < 100:
+        return "Високо"
+    elif level >= 101:
+        return "Многу високо"
 
 
 def notify(opstina,nivo,desc):
-	if int(round(float(nivo))) < 50:
-		print("Ниско." + nivo)
-		logger.info('Ниско за {} со индекс {} на AQI скала'.format(opstina,int(round(float(nivo)))))
-	else: 
-		print(message) 
-		logger.info('{} за {} со индекс {} на AQI скала'.format(desc,opstina,int(round(float(nivo)))))
-		status = api.update_status(status=message)
-		time.sleep(5)
-		pingstatus = api.update_status(status=pingmessage,in_reply_to_status_id=status.id_str)
+    if int(round(float(nivo))) < 50:
+        print("Ниско." + nivo)
+        logger.info('Ниско за {} со индекс {} на AQI скала'.format(opstina,int(round(float(nivo)))))
+    else:
+        if opstina == "lisice":
+            message = "Еј @OpstinaAerodrom! Индексот на загаденост во Лисиче во моментот е: {}. {} според AQI индексот.".format(desc,int(round(float(nivo))))
+            send_tweet(message)
+        elif opstina == "kumanovo":
+            message = "Еј @makraduli! Индексот на загаденост во Куманово во моментот е: {}. {} според AQI индексот.".format(desc,int(round(float(nivo))))
+            ping_message = "@eko_svest @skopjesmogalarm пинг ^"
+            send_tweet(message)
+        else:
+            message = "Еј @opstina{}! Индексот на загаденост во моментот е: {}. {} според AQI индексот.".format(opstina,desc,int(round(float(nivo))))
+            print(message)
+            logger.info('{} за {} со индекс {} на AQI скала'.format(desc,opstina,int(round(float(nivo)))))
+            send_tweet(message)
+
+def send_tweet(msg):
+    status = api.update_status(status=msg)
+    time.sleep(5)
+    pingstatus = api.update_status(status=pingmessage, in_reply_to_status_id=status.id_str)
 
 def main():
-	for place in places:
-		res = requests.get(url + place)
-		data = json.loads(res.text)
-		pollution_level = data[0]["data"]
-		description = index_value(pollution_level)
-		notify(place,pollution_level,description)
+    for place in places:
+        res = requests.get(url + place)
+        data = json.loads(res.text)
+        pollution_level = data[0]["data"]
+        description = index_value(pollution_level)
+        notify(place,pollution_level,description)
 
 main()
 
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 log_file = "dosadenko.log"
-places = ["karpos","centar","gazibaba"]
+places = ["karpos","centar","gazibaba","lisice","kumanovo"]
 url = "http://gorjan.rocks/clients/airquality/returnNewDataWeb.php?lastX=10&type=AQI&location="
 pingmessage = "@gradskopje1 @makraduli @eko_svest @skopjesmogalarm пинг ^"
 clientKey = ""
